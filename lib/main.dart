824fcf751c67221a03ef93764bc1a175cd32814a
shivshankarmh <shivshankarmh0104@gmail.comm>
2021-04-24 20:09:19 +0530

    login page

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {},
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(10),
+      color: Colors.blue,
+      child: MaterialButton(
+        onPressed: () {},
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        child: Text(
+          'Sign in',
+          style: GoogleFonts.montserrat(
+            fontSize: 20,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+        ),
+        minWidth: MediaQuery.of(context).size.width,
+      ),
+    );
+
+    final txtField1 = TextField(
+      textInputAction: TextInputAction.next,
+      obscureText: false,
+      style: GoogleFonts.montserrat(
+        fontSize: 20.0,
+      ),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: 'Email',
+        border: OutlineInputBorder(),
+      ),
+    );
+
+    final txtField2 = TextField(
+      obscureText: true,
+      style: GoogleFonts.montserrat(
+        fontSize: 20.0,
+      ),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: 'Password',
+        border: OutlineInputBorder(),
+      ),
+    );
+
+    final img = Image.asset(
+      'assets/1.png',
+    );
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              img,
+              txtField1,
+              txtField2,
+              loginButton,
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
